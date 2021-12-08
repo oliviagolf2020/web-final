c06fbb458eb53a548679c4428303a2fc287be079
Olivia Worsham <Olivia.Worsham@sckans.edu>
2021-12-08 16:12:27 -0600

    edit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 
 import Home from './home';
-import Olivia from './olivia';
+
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      <Olivia/>    
+          
       
     </div>
   );
